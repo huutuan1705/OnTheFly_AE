4847e3598feb16154b62bede7f3a6bf681db66ad
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-08-07 17:09:01 +0700

    update loss and main

--- baseline/utils.py
+++ baseline/utils.py
@@ -57,7 +57,7 @@ def loss_fn(args, features):
     infonce_positive = info_nce_loss(args, positive_feature_1, positive_feature_2)
     
     total_loss = triplet_loss_1 + triplet_loss_2 + mse_loss_1 + mse_loss_2 # + 0.1*infonce_sketch + 0.1*infonce_positive  # + mse_loss_1 + mse_loss_2
-    # total_loss = torch.mean(total_loss)
+    total_loss = torch.mean(total_loss)
     return total_loss
     
 def get_transform(type, aug_mode='geometric_strong'):
