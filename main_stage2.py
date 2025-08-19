e9d4352987f5985ea07c91aef65e6501d7810726
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-08-19 15:11:09 +0700

    update main and train

---
+++ main_stage2.py
@@ -0,0 +1,45 @@
+import torch
+import argparse
+from stage2.model import Siamese_SBIR
+from stage2.train import train_model
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+if __name__ == "__main__":
+    parsers = argparse.ArgumentParser(description='Baseline Fine-Grained SBIR model')
+    parsers.add_argument('--dataset_name', type=str, default='ShoeV2')
+    parsers.add_argument('--output_size', type=int, default=64)
+    parsers.add_argument('--num_heads', type=int, default=8)
+    parsers.add_argument('--root_dir', type=str, default='/kaggle/input/fg-sbir-dataset')
+    parsers.add_argument('--feature_dir', type=str, default='/kaggle/input/fg-sbir-dataset')
+    parsers.add_argument('--pretrained_dir', type=str, default='/kaggle/input/base_ae_model/pytorch/default/1/best_model.pth')
+    
+    parsers.add_argument('--use_kaiming_init', type=bool, default=True)
+    parsers.add_argument('--load_pretrained', type=bool, default=False)
+    
+    parsers.add_argument('--batch_size', type=int, default=1)
+    parsers.add_argument('--test_batch_size', type=int, default=1)
+    parsers.add_argument('--step_size', type=int, default=100)
+    parsers.add_argument('--gamma', type=float, default=0.5)
+    parsers.add_argument('--margin', type=float, default=0.3)
+    parsers.add_argument('--alpha', type=float, default=0.4)
+    parsers.add_argument('--temperature', default=0.07, type=float, help='softmax temperature (default: 0.07)')
+    parsers.add_argument('--threads', type=int, default=4)
+    parsers.add_argument('--lr', type=float, default=0.0001)
+    parsers.add_argument('--epochs', type=int, default=200)
+    
+    args = parsers.parse_args()
+    model = Siamese_SBIR(args).to(device)
+    
+    backbones_state = torch.load(args.feature_dir + "/" + args.dataset_name + "_backbone.pth", weights_only=True)
+    attention_state = torch.load(args.feature_dir + "/" + args.dataset_name + "_attention.pth", weights_only=True)
+    linear_state = torch.load(args.feature_dir + "/" + args.dataset_name + "_linear.pth", weights_only=True)
+    
+    model.sample_embedding_network.load_state_dict(backbones_state['sample_embedding_network'], strict=False)
+    model.sketch_embedding_network.load_state_dict(backbones_state['sketch_embedding_network'], strict=False)
+    model.attention.load_state_dict(attention_state['attention'], strict=False)
+    model.sketch_attention.load_state_dict(attention_state['sketch_attention'], strict=False)
+    model.linear.load_state_dict(attention_state['linear'], strict=False)
+    model.sketch_linear.load_state_dict(attention_state['sketch_linear'], strict=False)
+        
+    train_model(model, args)
--- stage2/train.py
+++ stage2/train.py
@@ -155,7 +155,7 @@ def train_model(model, args):
             loss_step += loss_triplet_1 + loss_triplet_2 + loss_info_nce
             loss_buffer.append(loss_step)
             
-            if (i + 1) % 20 == 0 or i == len(dataloader_train)-1: # Update after every 20 images or end training dataset
+            if (i + 1) % 20 == 0 or i == len(dataloader_train)-1: # Update after every 20 images or finish training dataset
                 optimizer.zero_grad()
                 policy_loss = torch.stack(loss_buffer).mean()
                 policy_loss.backward()
