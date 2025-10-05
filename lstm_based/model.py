2e349bd274f4a11a7b0148922364d6c7d8b809f4
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-10-05 09:48:10 +0700

    update lstm model

--- lstm_based/model.py
+++ lstm_based/model.py
@@ -57,8 +57,8 @@ class BiLSTM_Module(nn.Module):
 class Model(nn.Module):
     def __init__(self, args):
         super(Model, self).__init__()
-        train_pickle = os.path.join(args.root_dir, 'train_' + args.dataset_name +'.pickle')
-        test_pickle = os.path.join(args.root_dir, 'test_' + args.dataset_name +'.pickle')
+        train_pickle = os.path.join(args.pickle_dir, 'train_' + args.dataset_name +'.pickle')
+        test_pickle = os.path.join(args.pickle_dir, 'test_' + args.dataset_name +'.pickle')
         with open(train_pickle, "rb") as f:
             self.Image_Array_Train, self.Sketch_Array_Train, self.Image_Name_Train, self.Sketch_Name_Train = pickle.load(f)
         with open(test_pickle, "rb") as f:
