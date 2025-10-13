c77eb9838139063b31d9784a392e0c15ff3a8d2f
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-10-13 21:58:08 +0700

    update utils

--- baseline/attention.py
+++ baseline/attention.py
@@ -30,6 +30,27 @@ class SelfAttention(nn.Module):
         for x in self.parameters():
             x.requires_grad = False
 
+class Attention_global(nn.Module):
+    def __init__(self):
+        super(Attention_global, self).__init__()
+        self.pool_method =  nn.AdaptiveMaxPool2d(1) # as default
+        self.net = nn.Sequential(nn.Conv2d(2048, 512, kernel_size=1),
+                                 nn.BatchNorm2d(512),
+                                 nn.ReLU(),
+                                 nn.Conv2d(512, 1, kernel_size=1))
+       
+
+        
+    def forward(self, backbone_tensor):
+        backbone_tensor_1 = self.net(backbone_tensor)
+        backbone_tensor_1 = backbone_tensor_1.view(backbone_tensor_1.size(0), -1)
+        backbone_tensor_1 = nn.Softmax(dim=1)(backbone_tensor_1)
+        backbone_tensor_1 = backbone_tensor_1.view(backbone_tensor_1.size(0), 1, backbone_tensor.size(2), backbone_tensor.size(3))
+        fatt = backbone_tensor*backbone_tensor_1
+        fatt1 = backbone_tensor +fatt
+        fatt1 = self.pool_method(fatt1).view(-1, 2048)
+        return  F.normalize(fatt1)
+    
 class SketchAttention(nn.Module):
     def __init__(self, args):
         super(SketchAttention, self).__init__()
--- baseline/utils.py
+++ baseline/utils.py
@@ -73,14 +73,14 @@ def get_transform(type, aug_mode=1):
         elif aug_mode == 2:
             # Focus on cutout augmentation
             transform_list = [
-                # transforms.Resize(299),  # Weaker crop
-                transforms.RandomResizedCrop(299, scale=(0.85, 1.0)),
+                transforms.Resize(299),  # Weaker crop
+                # transforms.RandomResizedCrop(299, scale=(0.85, 1.0)),
                 # transforms.RandomRotation(5),  # Weaker rotation
-                # transforms.RandomApply([strong_color_jitter], p=0.8),
+                transforms.RandomApply([strong_color_jitter], p=0.8),
                 # transforms.RandomGrayscale(p=0.5),  # Add grayscale
                 # transforms.RandomApply([transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0))], p=0.8),
                 transforms.ToTensor(),
-                transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0),
+                # transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ]
             
@@ -101,8 +101,8 @@ def get_transform(type, aug_mode=1):
             transform_list = [
                 transforms.RandomResizedCrop(299, scale=(0.85, 1.0)),
                 transforms.RandomHorizontalFlip(0.5),
-                transforms.ColorJitter(brightness=0.15, contrast=0.15, saturation=0.15),
-                transforms.RandomRotation(15),
+                # transforms.ColorJitter(brightness=0.15, contrast=0.15, saturation=0.15),
+                # transforms.RandomRotation(15),
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ]
