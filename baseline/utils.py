129f3a699ba8dcc94c79a2bb10fc87f823c8e4a5
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-10-15 15:38:24 +0700

    update utils

--- baseline/utils.py
+++ baseline/utils.py
@@ -69,7 +69,7 @@ def get_transform(type, aug_mode=1):
                 # transforms.RandomGrayscale(p=0.5),  # Add grayscale
                 # transforms.RandomApply([transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0))], p=0.8),
                 transforms.ToTensor(),
-                transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0),
+                # transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ]
             
@@ -81,6 +81,7 @@ def get_transform(type, aug_mode=1):
                 transforms.RandomGrayscale(p=0.5),  # Add grayscale
                 transforms.RandomApply([transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0))], p=0.8),
                 transforms.ToTensor(),
+                transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ]
             
