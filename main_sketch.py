3bba62bde7552e504b9794b58a2d94642087f196
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-10-21 20:48:09 +0700

    add new inferences

--- main_sketch.py
+++ main_sketch.py
@@ -2,7 +2,8 @@ import torch
 import argparse
 from phase2.model import Siamese_SBIR
 from phase2.train import train_model
-from extract_features import Environtment
+from phase2.inferences import inference_model
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -17,11 +18,11 @@ if __name__ == "__main__":
     
     parsers.add_argument('--use_kaiming_init', type=bool, default=True)
     parsers.add_argument('--load_pretrained', type=bool, default=False)
-    parsers.add_argument('--stage2', type=bool, default=False)
+    parsers.add_argument('--inference', type=bool, default=False)
     
     parsers.add_argument('--batch_size', type=int, default=48)
     parsers.add_argument('--test_batch_size', type=int, default=1)
-    parsers.add_argument('--step_size', type=int, default=100)
+    parsers.add_argument('--steps', type=int, default=20)
     parsers.add_argument('--gamma', type=float, default=0.5)
     parsers.add_argument('--margin', type=float, default=0.3)
     parsers.add_argument('--alpha', type=float, default=0.4)
@@ -32,10 +33,21 @@ if __name__ == "__main__":
     
     args = parsers.parse_args()
     model = Siamese_SBIR(args).to(device)
-    envi = Environtment(args)
     
-    if args.load_pretrained is False:
-        linear_state = torch.load(args.pretrained_dir + "/" + args.dataset_name + "_linear.pth", weights_only=True)
-        model.attn.proj.load_state_dict(linear_state['sketch_linear'], strict=False)
+    if args.inference:
+        inference_model(model, args)
     
-    train_model(model, args)
+    else:
+        if args.load_pretrained is False:
+            backbones_state = torch.load(args.pretrained_dir + "/" + args.dataset_name + "_top5_backbone.pth", weights_only=True)
+            attention_state = torch.load(args.pretrained_dir + "/" + args.dataset_name + "_top5_attention.pth", weights_only=True)
+            linear_state = torch.load(args.pretrained_dir + "/" + args.dataset_name + "_top5_linear.pth", weights_only=True)
+
+            model.sample_embedding_network.load_state_dict(backbones_state['sample_embedding_network'], strict=False)
+            model.attention.load_state_dict(attention_state['attention'], strict=False)
+            model.linear.load_state_dict(linear_state['linear'])
+            model.sketch_embedding_network.load_state_dict(backbones_state['sketch_embedding_network'], strict=False)
+            model.sketch_attention.load_state_dict(attention_state['sketch_attention'], strict=False)
+            model.attn.proj.load_state_dict(linear_state['sketch_linear'], strict=False)
+        
+        train_model(model, args)
