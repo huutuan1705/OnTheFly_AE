9616750d29ae0200d567de4d5fc7717440efdd57
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-10-21 08:15:45 +0700

    update utils

--- baseline/utils.py
+++ baseline/utils.py
@@ -63,13 +63,9 @@ def get_transform(type, aug_mode=1):
             # Focus on shaped augmentation
             transform_list = [
                 transforms.RandomResizedCrop(299, scale=(0.85, 1.0)),
-                # transforms.RandomHorizontalFlip(0.5),
-                # transforms.RandomRotation(15),
-                # transforms.RandomApply([strong_color_jitter], p=0.8),
-                # transforms.RandomGrayscale(p=0.5),  # Add grayscale
-                # transforms.RandomApply([transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0))], p=0.8),
+                transforms.RandomHorizontalFlip(0.5),
+                transforms.RandomRotation(15),
                 transforms.ToTensor(),
-                # transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ]
             
@@ -77,8 +73,6 @@ def get_transform(type, aug_mode=1):
             # Focus on color augmentation
             transform_list = [
                 transforms.Resize(299), 
-                transforms.RandomRotation(15),
-                transforms.RandomHorizontalFlip(0.5),
                 transforms.RandomApply([strong_color_jitter], p=0.8),
                 transforms.RandomGrayscale(p=0.5),  # Add grayscale
                 transforms.RandomApply([transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0))], p=0.8),
