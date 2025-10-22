e4e90f3f54151d6414f1c51e375f340228bfd1e5
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-10-22 17:10:17 +0700

    update infer

--- phase2/inferences.py
+++ phase2/inferences.py
@@ -93,7 +93,7 @@ def evaluate_model(model, dataloader_test):
                     mean_rank[i_sketch] += 1/rank_all[i_batch, i_sketch].item()
                     mean_rank_percentile[i_sketch] += rank_all_percentile[i_batch, i_sketch].item()
             
-        print(rank_all[22:, :])
+        print(rank_all[22, :].numpy())
     
 def inference_model(model, args):
     model = model.to(device)
