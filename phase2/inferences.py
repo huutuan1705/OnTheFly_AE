5a0acb2b473f42b19b5b1ab2ac6fbf87e4ab04f3
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-11-10 09:37:28 +0700

    update refgerence

--- phase2/inferences.py
+++ phase2/inferences.py
@@ -29,69 +29,64 @@ def evaluate_model(model, dataloader_test):
             # print(batch['sketch_imgs'].shape) # (1, 25, 3, 299, 299)
             
             for data_sketch in batch['sketch_imgs']:
-                sketch_feature = model.sketch_embedding_network(
-                    data_sketch.to(device))
-                sketch_feature = model.sketch_attention(sketch_feature)
-                # sketch_feature = model.sketch_linear(sketch_feature)
+                sketch_feature = model.sketch_embedding_network(data_sketch.to(device))
                 
-                # print("sketch_feature.shape: ", sketch_feature.shape) #(25, 2048)
-                sketch_features_all = torch.cat(
-                    (sketch_features_all, sketch_feature.detach()))
+                sketch_feature = model.sketch_attention(sketch_feature)
+                sketch_features_all = torch.cat((sketch_features_all, sketch_feature.detach()))
 
             # print("sketch_feature_ALL.shape: ", sketch_features_all.shape) # (25, 2048)
             sketch_array_tests.append(sketch_features_all)
             sketch_names.extend(batch['sketch_path'])
 
             if batch['positive_path'][0] not in image_names:
-                positive_feature = model.sample_embedding_network(
-                    batch['positive_img'].to(device))
-                positive_feature = model.linear(
-                    model.attention(positive_feature))
+                positive_feature = model.sample_embedding_network(batch['positive_img'].to(device))
+                positive_feature = model.linear(model.attention(positive_feature))
                 
-                image_array_tests = torch.cat(
-                    (image_array_tests, positive_feature))
+                image_array_tests = torch.cat((image_array_tests, positive_feature))
                 image_names.extend(batch['positive_path'])
 
         # print("sketch_array_tests[0].shape", sketch_array_tests[0].shape) #(25, 2048)
         num_steps = len(sketch_array_tests[0])
-        mean_rank = torch.zeros(num_steps, dtype=torch.float64, device=device)
-        mean_rank_percentile = torch.zeros(num_steps, dtype=torch.float64, device=device)
+        avererage_area = []
+        avererage_area_percentile = []
+        sketch_range = []
         
         rank_all = torch.zeros(len(sketch_array_tests), num_steps)
         rank_all_percentile = torch.zeros(len(sketch_array_tests), num_steps)
+        sketch_range = torch.Tensor(sketch_range)
         
         for i_batch, sampled_batch in enumerate(sketch_array_tests):
-            
+            mean_rank = []
+            mean_rank_percentile = []
             sketch_name = sketch_names[i_batch]
 
             sketch_query_name = '_'.join(
                 sketch_name.split('/')[-1].split('_')[:-1])
             position_query = image_names.index(sketch_query_name)
             sketch_features = model.attn(sampled_batch)
-            # sketch_features = sampled_batch
-            if "CHARUF012YEL-UK_v1_MustardYellow" in sketch_query_name:
-                print(sketch_name)
-                print(i_batch)
-                
+
             for i_sketch in range(sampled_batch.shape[0]):
-                # print("sketch_features[i_sketch].shape: ", sketch_features[i_sketch].shape)
                 sketch_feature = sketch_features[i_sketch]
                 target_distance = F.pairwise_distance(sketch_feature.to(device), image_array_tests[position_query].to(device))
                 distance = F.pairwise_distance(sketch_feature.unsqueeze(0).to(device), image_array_tests.to(device))
                 
-                if sketch_name == "/test/CHARUF012YEL-UK_v1_MustardYellow_12":
-                    sorted_dist, sorted_idx = torch.sort(distance)
-                    top_idx = sorted_idx[:10].tolist()
-                    top_names = [image_names[i] for i in top_idx]
-                    
-                    print(i_sketch)
-                    print(top_names)
-                
                 rank_all[i_batch, i_sketch] = distance.le(target_distance).sum()
                 rank_all_percentile[i_batch, i_sketch] = (len(distance) - rank_all[i_batch, i_sketch]) / (len(distance) - 1)
                 
-            
-        print(rank_all[22, :].numpy())
+                if rank_all[i_batch, i_sketch].item() == 0:
+                    mean_rank.append(1.)
+                else:
+                    mean_rank.append(1/rank_all[i_batch, i_sketch].item())
+                    # 1/(rank)
+                    mean_rank_percentile.append(rank_all_percentile[i_batch, i_sketch].item())
+                    
+            avererage_area.append(mean_rank)
+            avererage_area_percentile.append(mean_rank_percentile)
+
+        meanA = [sum(col) / len(col) for col in zip(*avererage_area)]
+        meanB = [sum(col) / len(col) for col in zip(*avererage_area_percentile)]
+        
+        return meanA, meanB
     
 def inference_model(model, args):
     model = model.to(device)
